34f6b9cf662ed8f3e9b8562293e4722937dfa70a
usac201603188 <3001784840101@ingenieria.usac.edu.gt>
2020-06-15 09:16:46 -0600

    Multiplicacion entre matrices finalizada

--- T02/OOP.py
+++ T02/OOP.py
@@ -28,7 +28,7 @@ class matriz(object):
         else:
             raise ErrorDimensional(self.dimensions(), sumando.dimensions())
 
-    def __sub__(self, sustraendo):
+    def __sub__(self, sustraendo):              #Sobrecarga del operador resta, aplicado a matrices
         if self.equalLenghts(sustraendo):
             result=[]
             for i in range(self.dimensions()[0]):
@@ -40,37 +40,54 @@ class matriz(object):
         else:
             raise ErrorDimensional(self.dimensions(), sustraendo.dimensions())
 
-    def multiHelper(self, ayudaPLS):
+    def multiHelper(self, ayudaPLS):            #metodo que crea un vector con 0 para que sea llenado despues
         newList=[]
         for i in range(ayudaPLS):
             newList.append(0)
         return newList
 
-    def __mul__(self, multiplicador):
-        if type(multiplicador)==int or type(multiplicador)==float:
+    def sumaHelp(self, vect1, vect2):           #Este metodo suma los vectores que crea cada posicion para poder ser
+        result=[]                               #la multiplicacion entre matrices
+        for i in range(len(vect1)):
+            new_result=vect1[i]+vect2[i]
+            result.append(new_result)    
+        
+        return result           
+
+    def __mul__(self, multiplicador):           #Metodo de sobrecarga del operador multiplicacion, apilcado a matrices
+        if type(multiplicador)==int or type(multiplicador)==float:      #Se diferencia si es una matriz por escalar
             result=[]
             for i in range(self.dimensions()[0]):
                 fila=[]
-                for j in range(self.dimensions()[1]):
+                for j in range(self.dimensions()[1]):               #se recorre sencillamente el metodo para multiplicar uno a uno
                     fila.append(self.data[i][j]*multiplicador)
                 result.append(fila)
             return matriz(result)
-        elif type(multiplicador)==list or type(multiplicador)==matriz:
-            if (self.dimensions()[1]==multiplicador.dimensions()[0]):
+        elif type(multiplicador)==list or type(multiplicador)==matriz:  #Se diferencia si se trabajará con matrices
+            #return ("Lamentamos las fallas tecnicas")
+            #"""
+            #if type(multiplicador)==list:                              #Se debe realizar una conversion a tipo matriz pero mi neurona ya no da mas
+                #self.multiplicador=list(multiplicador)
+            if (self.dimensions()[1]==multiplicador.dimensions()[0]):   #Para poder multiplicarlo deben cumplir con esta condicion, columnas1=filas2
                 result=[]
                 for i in range(self.dimensions()[0]):
-                    fila=[]
-                    helper=self.multiHelper(self.dimensions()[0])
+                    #fila=[]
+                    hell_per=self.multiHelper(self.dimensions()[0])
                     for j in range(self.dimensions()[1]):
                         multi=[]
-                        for k in range(multiplicador.dimensions()[1]):
-                            multi.append(a[i][j]*b[j][k])
-                        fila.append(helper=helper+multi)
-                    result.append(fila)
+                        #new_var=0
+                        for k in range(multiplicador.dimensions()[1]):  #Se recorren las matrices y se realiza la respectiva multiplicacion
+                            #new_var=self.data[i][j]*multiplicador.data[j][k]
+                            multi.append(self.data[i][j]*multiplicador.data[j][k])
+                        hell_per=self.sumaHelp(hell_per,multi)
+                        #fila.append(hell_per)
+                    
+                    result.append(hell_per) 
                 return matriz(result)
-
+            
             else:
                 raise ErrorDimensional(self.dimensions(), multiplicador.dimensions())
+            #"""
         else:
             raise TypeError("Ingresar campos validos a mutliplicar")
 
@@ -108,6 +125,6 @@ d=matriz([[1],[2],[3]])
 e=matriz([[1,7],[0,2]])
 f=matriz([1,2,3,5])
 g=matriz([5])
-print("prueba2")
+print("prueba_definitiva")
 
  
